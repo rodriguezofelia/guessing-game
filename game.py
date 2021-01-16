cc1002d0af02fce2a18890e70a91fbedc769fe88
Ofelia <oer.rodriguez@gmail.com>
2021-01-16 20:35:33 +0000

    completed logic for number guessing game
    Co-authored by: Grace Birnam <grace.mbirnam@gmail.com>

--- game.py
+++ game.py
@@ -10,26 +10,33 @@ import random
 #         increase number of guesses
 #     else:
 #         congratulate player
-count = 1
-random_num = random.randint(0,100)
-name = input("What is your name? >")
-number = int(input("Enter a number between 1 - 100 >"))
-print(f"Welcome {name}")
+def guessing_game():
 
-while number >= 1 and number <= 100:
-    if number != random_num and number < random_num:
-        print("Your guess is too low, try again! ")
-        break
-        count += 1
+    name = input("What is your name? >")
+    print(f"Welcome {name}")
+    random_num = random.randint(0,100)
+    print(random_num)
+    count = 1
 
-    elif number != random_num and number > random_num:
-        print("Your guess is too high, try again!")
-        break
-        count += 1
+    while True:
+        number = int(input("Enter a number between 1 - 100 >"))
+        
+        if number >= 1 and number <= 100:
+            if number != random_num and number < random_num:
+                print("Your guess is too low, try again! ")
+                count += 1
 
-    else:
-        print(f"Well done {name}! You found my number in {count} tries!")
-        break
+            elif number != random_num and number > random_num:
+                print("Your guess is too high, try again!")
+                count += 1
+
+            else:
+                print(f"Well done {name}! You found my number in {count} tries!")
+                break
+        else: 
+            print("Sorry, that's not a number between 1 and 100!")
+
+guessing_game()
 
 
     
