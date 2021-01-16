da947bb4829af0243a3d9075b58b999b05f91b79
Ofelia <oer.rodriguez@gmail.com>
2021-01-16 20:02:28 +0000

    creating a guessing game

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 """A number-guessing game."""
-
+import random
 # greet player
 # get player name
 # choose random number between 1 and 100
@@ -10,6 +10,29 @@
 #         increase number of guesses
 #     else:
 #         congratulate player
+count = 1
+random_num = random.randint(0,100)
 name = input("What is your name? >")
+number = int(input("Enter a number between 1 - 100 >"))
 print(f"Welcome {name}")
-number = int(input"Enter a number between 1 - 100 >")
+
+while number >= 1 and number <= 100:
+    if number != random_num and number < random_num:
+        print("Your guess is too low, try again! ")
+        break
+        count += 1
+
+    elif number != random_num and number > random_num:
+        print("Your guess is too high, try again!")
+        break
+        count += 1
+
+    else:
+        print(f"Well done {name}! You found my number in {count} tries!")
+        break
+
+
+    
+    
+
+
